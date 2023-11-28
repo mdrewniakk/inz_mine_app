e4b7a92251a8b122a76b166fb401a1a4de3b1ffa
mdrewniakk <drewniakm78@wp.pl>
2023-11-28 15:14:03 +0100

    sd

---
+++ geemap-apps-master/pages/Dome_Mine.py
@@ -0,0 +1,216 @@
+import ee
+import streamlit as st
+import geemap.foliumap as geemap
+import plotly.express as px
+import plotly.graph_objects as go
+import extra_streamlit_components as stx
+
+import numpy as np
+import geemap.colormaps as cm
+
+# Get an NLCD image by year.
+
+ee.Initialize()
+
+def calculate_ndvi(image):
+    return (image.select('B8').subtract(image.select('B4'))).divide(image.select('B8').add(image.select('B4'))).rename('NDVI').set('year', ee.Date(image.get('system:time_start')).get('year'))
+def calculate_ndwi1(image):
+    return (image.select('B8A').subtract(image.select('B12'))).divide(image.select('B8A').add(image.select('B12'))).rename('NDWI1').set('year', ee.Date(image.get('system:time_start')).get('year'))
+def calculate_ndwi2(image):
+    return (image.select('B3').subtract(image.select('B8'))).divide(image.select('B8').add(image.select('B3'))).rename('NDWI2').set('year', ee.Date(image.get('system:time_start')).get('year'))
+def calculate_evi(image):
+    evi = image.expression(
+    '2.5 * (NIR - RED) / (NIR + 6 * RED - 7.5 * BLUE + 1)', {
+      'NIR': image.select('B8'),
+      'RED': image.select('B4'),
+      'BLUE': image.select('B2')
+    })
+    return evi.rename('EVI').set('year', ee.Date(image.get('system:time_start')).get('year'))
+def calculate_nmdi(image):
+    nmdi = image.expression(
+        '(NIR - (SWIR1-SWIR2)) / (NIR + (SWIR1 - SWIR2))', {
+            'NIR': image.select('B8A'),
+            'SWIR1': image.select('B11'),
+            'SWIR2': image.select('B12') })
+    return nmdi.rename("NMDI").set('year', ee.Date(image.get('system:time_start')).get('year'))
+
+def calculate_msavi(image):
+    msavi = image.expression(
+        '(2*NIR + 1 - sqrt((2*NIR+1)^2-8*(NIR-RED)))/2', {
+            'NIR' : image.select('B8'),
+            'RED' : image.select('B4') })
+    return msavi.rename('MSAVI2').set('year', ee.Date(image.get('system:time_start')).get('year'))
+def calculate_msi(image):
+    msi=image.select('B11').divide(image.select('B8A'))
+    return msi.rename('MSI').set('year', ee.Date(image.get('system:time_start')).get('year'))
+
+def best_image(year):
+    start_date = ee.Date.fromYMD(year, 1, 1)
+    end_date = start_date.advance(1, 'year')
+    image = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED') \
+        .filterBounds(ROI) \
+        .filterDate(start_date, end_date) \
+        .sort("CLOUDY_PIXEL_PERCENTAGE") \
+        .first()
+    return image
+def calc_indices(images, bound):
+    l = []
+    for index in range(0, count):
+        image = ee.Image(images.get(index)).clipToCollection(bound)
+        im = calculate_ndvi(image).addBands(calculate_ndwi1(image)).addBands(calculate_ndwi2(image)).addBands(calculate_nmdi(image)).addBands(calculate_evi(image)).addBands(calculate_msi(image))
+        l.append(im)
+    col = ee.ImageCollection.fromImages(l)
+    return col
+ndwi_vis = {
+    'min': -1,
+    'max': 1,
+    'palette': ['ff3f06', 'f0ff06', '#006400', '0afff4', '0625ff'],
+}
+ndvi_vis = {
+    'min': 0,
+    'max': 1,
+    'palette': 'ndvi',
+}
+nmdi_vis = {
+    'min': -0.5,
+    'max': 1,
+    'palette': 'RdYlGn',
+}
+
+params = {"NDWI1": ndwi_vis, "NDVI": ndvi_vis, "NMDI": nmdi_vis}
+def get_params(index):
+    return params[index]
+
+
+def lineplot(index):
+    means = []
+    medians = []
+    modes = []
+    mean_value = dataset.select(index).toBands().reduceRegion(
+        reducer=reducer,
+        geometry=dome,
+        scale=30  # Adjust the scale as needed
+    )
+    for i in range(0, 6):
+        means.append(mean_value.get(f"{i}_{index}_mean").getInfo())
+        medians.append(mean_value.get(f"{i}_{index}_median").getInfo())
+        modes.append(mean_value.get(f"{i}_{index}_mode").getInfo())
+    fig = go.Figure()
+
+    # Add traces for means, medians, and modes
+    fig.add_trace(go.Scatter(x=years, y=means, mode='lines+markers', name='Średnia'))
+    fig.add_trace(go.Scatter(x=years, y=medians, mode='lines+markers', name='Mediana'))
+    fig.add_trace(go.Scatter(x=years, y=modes, mode='lines+markers', name='Moda'))
+
+    # Update layout
+    fig.update_layout(
+        title=f'Zmiana statystyk indeksu {index} na przestrzeni lat',
+        xaxis_title="Rok",
+        yaxis_title='Wartość',
+        showlegend=True
+    )
+
+    # Show the Plotly chart
+    st.plotly_chart(fig, use_container_width=True)
+palettes_gee = {"NDWI1": 'RdYlBu', "NDVI": 'ndvi', "NMDI": 'YlGnBu'}
+palettes_hist = {"NDWI1": px.colors.diverging.RdYlBu, "NDVI": px.colors.diverging.RdYlGn, "NMDI": px.colors.sequential.YlGnBu}
+text = {"NDWI1": 'RdYlBu',
+        "NDVI": '<div style="text-align: justify;">Normalized Difference Vegetation Index (NDVI) to indeks wegetacyjny używany w zdalnym odczycie do oceny zdrowia roślin i ilości roślinności na powierzchni ziemi. Jest to jedno z najczęściej stosowanych narzędzi do monitorowania zmian w pokrywie roślinnej na podstawie obrazów satelitarnych lub lotniczych.</div>',
+        "NMDI": '<div style="text-align: justify;">Normalizowany Wielopasmowy Indeks Suszy (NMDI) to indeks zdalnego odczytu używany do oceny warunków suszy na podstawie obrazów satelitarnych. Zaprojektowany jest, aby uchwycić zmiany w zdrowiu roślin związane z dostępnością wody. NMDI zazwyczaj wykorzystuje dane z pasm bliskiej podczerwieni (NIR) i krótkofalowej podczerwieni (SWIR) z multispektralnych danych satelitarnych.</div>'}
+
+def get_minmax(year, index):
+    nlcd = dataset.filter(ee.Filter.eq("year", year)).first()
+    im = nlcd.select(index)
+    mean = im.reduceRegion(
+        ee.Reducer.mean(), scale=40).getInfo()[index]
+    sd = im.reduceRegion(
+        ee.Reducer.stdDev(), scale=40).getInfo()[index]
+    vis_param = {
+    'min': mean - (sd * 3),
+    'max': mean + (sd * 3),
+    'palette': palettes_gee[index],
+    }
+    return vis_param
+
+
+def get_index(year, index):
+    nlcd = dataset.filter(ee.Filter.eq("year", year)).first()
+    return nlcd.select(index)
+
+def latest_image():
+    image = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED') \
+        .filterBounds(ROI) \
+        .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 10)) \
+        .sort('system:time_start', False) \
+        .first()
+    return image
+
+
+mining = ee.FeatureCollection("projects/sat-io/open-datasets/global-mining/global_mining_polygons")
+Map = geemap.Map(center=(48.458284, -81.240261), zoom=13)
+ROI = ee.Geometry.Point(-81.240261, 48.458284)
+start_year = 2018
+end_year = 2022
+dome = mining.filter(ee.Filter.inList('AREA', [8.93758181, 3.93418783]))
+Map.addLayer(dome)
+years = ee.List.sequence(start_year, end_year)
+year_list = years.getInfo()
+images_canada = years.map(best_image)
+images_canada = images_canada.add(latest_image())
+count = images_canada.size().getInfo()
+dataset = calc_indices(images_canada, dome)
+st.header("KANADA CHUJ INDEKSYU I WGLE")
+
+# Create a layout containing two columns, one for the map and one for the layer dropdown list.
+row1_col1, row1_col2, row1_col3 = st.columns([1, 2, 1])
+
+
+# Create an interactive map
+reducer = ee.Reducer.mean().combine(
+        ee.Reducer.median(), sharedInputs=True).combine(ee.Reducer.mode(), sharedInputs=True)
+
+
+# Convert the reduced collection to a FeatureCollection
+
+
+# Select the seven NLCD epochs after 2000.
+years = [2018, 2019, 2020, 2021, 2022, 2023]
+indices = ['NDVI',  'EVI', 'NDWI1', 'NDWI2', 'NMDI', 'MSI', 'MSAVI2']
+with row1_col3:
+    year = st.selectbox("Select a year", years)
+    index = st.selectbox("Select a index", indices)
+    add_legend = st.checkbox("Show legend")
+    lineplot(index)
+
+if year:
+    if index:
+        Map.addLayer(get_index(year, index), get_minmax(year, index), index + str(year))
+
+        with (row1_col1):
+            st.markdown(text[index], unsafe_allow_html=True)
+            st.latex(r'''NMDI\:=\frac{\left(NIR-SWIR\right)}{\left(NIR\:+\:SWIR\right)}''')
+            values = get_index(year, index).reduceRegion(
+                reducer=ee.Reducer.toList(),
+                geometry=get_index(year, index).geometry(),
+                scale=40  # Adjust scale as needed
+            ).getInfo()[index]
+
+            # use numpy to generate histogram
+            BINS = 60
+            y, x = np.histogram(values, bins=BINS)
+            # bin edges to midpoint
+            x = [(a + b) / 2 for a, b in zip(x, x[1:])]
+            fig = px.bar(x=x, y=y, color=x, color_continuous_scale=palettes_hist[index]
+                         ).update_layout(title='Histogram rozkładu wartości indeksu', xaxis_title='Wartość', yaxis_title='Ilość wystąpień')
+            st.plotly_chart(fig, use_container_width=True)
+
+
+            if add_legend:
+                Map.add_colorbar(get_minmax(year, index), label=f"Wartość {index}", layer_name=index + str(year))
+            with row1_col2:
+                Map.to_streamlit(height=600)
+
+else:
+    with row1_col2:
+        Map.to_streamlit(height=600)
+
